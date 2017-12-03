1c02f038b93592d7854f03982d38d81916d24798
apocalypse575 <apocalypse575@gmail.com>
2017-12-03 03:49:15 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -5,5 +5,3 @@ end
 def introduction_with_language(name, language)
   puts "This is a #{name}, #{language}."
 end
-
-rspec ./spec/introduction_spec.rb:10 # #introduction_with_language takes in two arguments, a name and a language, and puts out a phrase using those
